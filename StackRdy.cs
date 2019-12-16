0170c0686a80cfdd8fb6a112ccf769975f87e09f
kostyanidze <47399633+kostyanidze@users.noreply.github.com>
2019-12-17 02:16:23 +0400

    Add files via upload

---
+++ StackRdy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class StackRdy<T>
+{
+    private List<T> items = new List<T>();
+
+    public int Count => items.Count;
+
+    public void push(T item)
+    {
+        items.Add(item);
+    }
+    
+    public T Pop()
+    {
+        if (Count > 0)
+        {
+            var item = items.LastOrDefault();
+            items.Remove(item);
+            return item;
+        }
+        else
+            throw new NullReferenceException("Stack is empty :C");
+    }   
+    public T Peek()
+    {
+        if (Count > 0)
+        {
+            var item = items.LastOrDefault();
+            items.Remove(item);
+            return item;
+        }
+        else
+            throw new NullReferenceException("Stack is empty :C");
+    }
+    public override string ToString()
+    {
+        return $"Stack with {Count} elements";
+    }
+}
